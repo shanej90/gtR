661af1a24a4b7fdd7ac42fd37ad0e987a1598c04
JFWay <J.F.Way@exeter.ac.uk>
2021-11-02 13:51:54 +0000

    Added new functions that querry the database with a combination of resources to refine results

---
+++ R/gtr_combinations.R
@@ -0,0 +1,15 @@
+#' List of endpoints combinations for query_resource_linked_res function.
+#'
+#' A list of endpoint combinations for the query_resource_linked_res function.
+#' @export
+
+gtr_combinations <- list(
+  "orginisation project",
+  "orginisation publication",
+  "orginisation fund",
+  "orginisation publication",
+  "person project",
+  "person publication",
+  "person fund",
+  "person publication"
+)
